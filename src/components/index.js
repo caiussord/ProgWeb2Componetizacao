216a984eeac92df02effa116bf62a1a5ed14e068
Caius Sordillo <141962436+caiussord@users.noreply.github.com>
2025-11-04 19:34:49 -0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    // Conteúdo a ser exibido
+    // JSX => html + xml (html com css dentro do js)
+    <header className={styles.header}>
+      <span>Aula Senac</span>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">Senac</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
